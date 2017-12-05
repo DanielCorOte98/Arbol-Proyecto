4332aa985f2c517ace8092febfa0efdd28f69907
MiguelPe2454 <miguelpa2454@gmail.com>
2017-12-05 12:14:20 -0600

    Subiendo metodo de MaximoMinimo

--- src/sample/ArbolBinario.java
+++ src/sample/ArbolBinario.java
@@ -1,5 +1,7 @@
 package sample;
 
+import java.awt.*;
+
 /**
  * Created by Carlos Daniel on 28/11/2017.
  */
@@ -24,6 +26,24 @@ public class ArbolBinario {
             postOrden ( raiz.getNodoIzquierdo () );
             postOrden ( raiz.getNodoDerecho () );
             System.out.println (raiz.getDato ()+ " ");
+        }
+    }
+
+    int menor = 9999999;
+    int mayor = -99999;
+    public void mayorMenor(Nodo raiz, Label label){
+        if (raiz != null){
+            mayorMenor(raiz.getNodoIzquierdo(),label);
+
+            mayorMenor(raiz.getNodoDerecho(), label);
+
+            if (raiz.getDato() >= mayor ){
+                mayor = raiz.getDato();
+            }
+            if (raiz.getDato() <= menor){
+                menor = raiz.getDato();
+            }
+            label.setText("Número mayor: "+  mayor + "\nNúmero menor: "+ menor);
         }
     }
 }
